455a338c71495aab907b6ad529ac8ad1fd18a9f4
Lukas Lunkwitz <77695529+Luke-lwz@users.noreply.github.com>
2024-06-04 11:33:50 +0000

    swag

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,70 @@
-
 interface Routes {
-    route: string; // /profile/:id
-    component: () => void;
-    subRoutes?: Routes[];
+  route: string; // /profile/:id
+  component: () => void;
+  subRoutes?: Routes[];
 }
-
-
 
 interface UIOptions {
-    prefix?: string; // default: 'ui' 
-    routeDirectory?: string; // default: '/ui'
-    customRoutes?: Routes[]; // when provided, directory is not used
+  prefix?: string; // default: 'ui' // maxLength = 12
+  routeDirectory?: string; // default: '/ui'
+  customRoutes?: Routes[]; // when provided, directory is not used
+  useFunctionalButtons?: boolean; // default: false
+  functionalButtonTtl?: number; // in seconds // default: 1800 (30 minutes)
 }
 
+const ARGS_DIVIDER = ">";
 
 export default function createUI(options: UIOptions) {
+  const {
+    prefix = "ui",
+    routeDirectory = "/ui",
+    customRoutes,
+    useFunctionalButtons = false,
+  } = options;
+  if (prefix.length > 12) {
+    throw new Error("Prefix length cannot be more than 12 characters");
+  }
+  if (prefix.includes(ARGS_DIVIDER)) {
+    throw new Error(`Prefix cannot contain '${ARGS_DIVIDER}' character`);
+  }
 
-  console.log('createUI');
+  const routePrefix = routeDirectory + prefix;
+  const routes: Routes[] =
+    customRoutes || getRoutesFromDirectory(routeDirectory);
+
+  function onInteraction(interaction: any) {
+    if (!interaction?.isButton() || !interaction?.isStringSelectMenu()) return;
+    const { customId = "" } = interaction || {};
+    const [_prefix, type, ...args] = customId.split(ARGS_DIVIDER);
+
+    if (_prefix !== prefix) return;
+
+    switch (type) {
+      case "n": // navigate
+        const [storeLocation, arg] = args;
+        switch (storeLocation) {
+          case "c": // cache
+            // ui>n>c>1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed
+            const id = arg;
+            break;
+          case "r": // route
+            // ui>n>r>/profile/123482938747191284 // happens when route is smaller than 100 characters
+            const route = arg;
+            break;
+          default:
+            break;
+        }
+        break;
+      case "f": // function
+        const [functionId] = args;
+        // ui>f>1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+function getRoutesFromDirectory(directory: string): Routes[] {
+  return [];
 }
