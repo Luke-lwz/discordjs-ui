1cfa3ec9fc7247fdddf61c0963c21efbaf9412ee
Lukas Lunkwitz <lukaslunkwitz@gmail.com>
2024-08-30 00:02:32 +0200

    doesnt work yet

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -3,20 +3,17 @@
 
 
 
-export function mergeLayout(baseLayout: any, merge: any) {
-    const outLayout = {};
+export function mergeLayout(baseLayout: any = {}, merge: any) {
 
-    function recurseThroughObject(baseLayout, merge) {
-        for (const key in baseLayout) {
-            if (typeof baseLayout[key] === 'object' && !Array.isArray(baseLayout[key])) {
-                outLayout[key] = {};
-                recurseThroughObject(baseLayout[key], merge[key]);
-            } else  { // if it's not an object
-                outLayout[key] = merge[key] || baseLayout[key];
-            }
-        }
+    const outObj = {
+        ...(baseLayout || {}),
     }
-    recurseThroughObject(baseLayout, merge);
+
+    for (const key in merge) {
+            outObj[key] = merge[key];
+    }
+
+    return outObj;
 
 
 }
