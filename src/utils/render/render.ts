ddb4dbd483e09e0efa3340bc79455637c482d585
Lukas Lunkwitz <lukaslunkwitz@gmail.com>
2024-08-29 21:09:59 +0200

    swag

--- src/utils/render/render.ts
+++ src/utils/render/render.ts
@@ -1,22 +1,14 @@
 import { UIRenderOptions } from "../../types";
 import { getContext } from "../context";
 
-async function render(msg: any, options: UIRenderOptions = { reply: false }) {
+async function render(msg: any) {
   const { interaction } = getContext();
 
-  const { reply } = options || {};
   // render UI
 
   // add default message and then do a update not put (content : null, embeds: []) per default 🚨
 
   try {
-    if (reply) {
-      if (interaction?.replied) {
-        await interaction?.followUp?.(msg);
-      } else {
-        await interaction?.reply?.(msg);
-      }
-    } else {
       if (interaction?.deffered || interaction?.replied) {
         await interaction?.editReply?.(msg);
       } else {
@@ -26,7 +18,7 @@ async function render(msg: any, options: UIRenderOptions = { reply: false }) {
           await interaction?.reply?.(msg);
         }
       }
-    }
+    
   } catch (e) {
     console.error(e);
   }
--- src/utils/render/reply.ts
+++ src/utils/render/reply.ts
@@ -1,7 +1,15 @@
+import { getContext } from "../context";
 import render from "./render";
 
 async function reply(msg: any) {
-    await render(msg, { reply: true });
+  const { interaction } = getContext();
+
+  if (interaction?.replied) {
+    await interaction?.followUp?.(msg);
+  } else {
+    await interaction?.reply?.(msg);
+  }
+
 }   
 
 export default reply;
